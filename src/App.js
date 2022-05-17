ce69c024f7fce8d92542506e2728b58e59b6ed43
wowinter13 <vla-dy@yandex.ru>
2022-05-17 14:37:43 +0400

    Add simple frontend (Section VI)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const httpTransport = axios.create({
+  baseURL: "https://api.sometestdomain.me/api/v1/",
+});
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      ping: "Waiting"
+    }
+  }
+
+  componentDidMount() {
+    httpTransport.get('/digits')
+      .then((res, err) => {
+        if(res.status === 200) {
+          this.setState({
+            digits: res.data.digits
+          })
+        }
+        else {
+          console.log(err);
+        }
+      })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {this.state.digits}
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
