03d6ce34aaeda661bb00317f1aa8a8895eae1ee2
wowinter13 <vla-dy@yandex.ru>
2022-06-02 17:23:41 +0400

    feat: refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
@@ -7,45 +7,33 @@ const httpTransport = axios.create({
   baseURL: "https://api.sometestdomain.me/api/v1/",
 });
 
-class App extends React.Component{
 
-  constructor(props){
-    super(props);
-    this.state = {
-      ping: "Waiting"
-    }
-  }
+export const App = () => {
+  const [digits, setDigits] = useState(0);
 
-  componentDidMount() {
+  useEffect(() => {
     httpTransport.get('/digits')
-      .then((res, err) => {
-        if(res.status === 200) {
-          this.setState({
-            digits: res.data.digits
-          })
-        }
-        else {
-          console.log(err);
-        }
-      })
-  }
+      .then((res) => setDigits(res.data.digits))
+      .catch(err => console.log(err));
+  }, []);
 
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {this.state.digits}
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+  return <div className="App">
+    <header className="App-header">
+      <img
+        src={logo}
+        className="App-logo"
+        alt="logo"
+      />
+      <a
+        href="#"
+        className="App-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {digits}
+      </a>
+    </header>
+  </div>;
+};
 
 export default App;
